01cf2588f047b6bb7da8790e0e0edf3433395f56
PeterR <solutionsgelo@gmail.com>
2019-07-23 17:19:49 +0200

    custom types

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { GraphQLServer } from 'graphql-yoga';
+
+const typeDefs = `
+    type Query {
+      me: User!
+    }
+
+    type User {
+      id: ID!
+      name: String!
+      email: String!
+      age: Int
+    }
+`;
+const resolvers = {
+  Query: {}
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log('Server Started!');
+});
