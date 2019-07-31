32ace394ec387f74a0020ef00edeb4af6801bca7
PeterR <solutionsgelo@gmail.com>
2019-07-31 11:38:42 +0200

    added update mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -34,6 +34,32 @@ const Mutation = {
     return deletedUsers[0];
   },
 
+  updateUser(parent, { id, data }, { db }, info) {
+    const user = db.users.find(user => user.id === id);
+
+    if (!user) {
+      throw new Error('User not found.');
+    }
+
+    if (typeof data.email === 'string') {
+      const emailTaken = db.users.some(user => user.email === data.email);
+      if (emailTaken) {
+        throw new Error('Email is already in use.');
+      }
+      user.email = data.email;
+    }
+
+    if (typeof data.name === 'string') {
+      user.name = data.name;
+    }
+
+    if (typeof data.age !== 'undefined') {
+      user.age = data.age;
+    }
+
+    return user;
+  },
+
   createPost(parent, { data }, { db }, info) {
     const existingUser = db.users.some(user => user.id === data.author);
     if (!existingUser) {
@@ -59,6 +85,22 @@ const Mutation = {
     db.comments = db.comments.filter(comment => comment.post !== id);
     return deletedPost[0];
   },
+  updatePost(parent, { id, data }, { db }, info) {
+    const post = db.posts.find(post => post.id === id);
+    if (!post) {
+      throw new Error("Post doesn't exist");
+    }
+    if (typeof data.title === 'string') {
+      post.title = data.title;
+    }
+    if (typeof data.body === 'string') {
+      post.body = data.body;
+    }
+    if (typeof data.published === 'boolean') {
+      post.published = data.published;
+    }
+    return post;
+  },
   createComment(parent, { data }, { db }, info) {
     const existingUserAndPost =
       db.users.some(user => user.id === data.author) &&
@@ -80,6 +122,16 @@ const Mutation = {
     }
     const deletedComment = db.comments.splice(commentId, 1);
     return deletedComment[0];
+  },
+  updateComment(parent, { id, data }, { db }, info) {
+    const comment = db.comments.find(comment => comment.id === id);
+    if (!comment) {
+      throw new Error("Comment doesn't exist.");
+    }
+    if (typeof data.body === 'string') {
+      comment.body = data.body;
+    }
+    return comment;
   }
 };
 
